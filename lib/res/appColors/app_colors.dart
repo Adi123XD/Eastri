a6b2e4189cc7645e474716ab565158db4622f8cf
unknown <mvmatharva4@gmail.com>
2025-01-31 18:19:37 +0530

    updated appcolors

--- lib/res/appColors/app_colors.dart
+++ lib/res/appColors/app_colors.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFFFFFFF);
-  static const Color secondaryColor = Color(0xFF23546d);
+  static const background = Colors.white;
+  static const signInbutton = Color(0xFF23546D);
+  static const signUpbutton = Colors.white;
+  static const title1 = Color(0xFF23546D);
+  static const title2 = Color(0x9C000000);
 }
