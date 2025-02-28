4b4682cbda8da88d8fb3355ce20471faf2eacb07
unknown <mvmatharva4@gmail.com>
2025-02-28 23:46:12 +0530

    final merge

--- lib/utils/appRoutes/app_routes.dart
+++ lib/utils/appRoutes/app_routes.dart
@@ -1,10 +1,15 @@
 import 'package:eastri_customer_app/views/screens/auth_screen.dart';
+import 'package:eastri_customer_app/views/screens/checkout_screen/checkout_screen.dart';
 import 'package:eastri_customer_app/views/screens/dashboard_screen/add_address_screen.dart';
 import 'package:eastri_customer_app/views/screens/dashboard_screen/dashboard_screen.dart';
 import 'package:eastri_customer_app/views/screens/onboarding_screen.dart';
 import 'package:eastri_customer_app/views/screens/otp_screen.dart';
+import 'package:eastri_customer_app/views/screens/pricing_screen.dart/iron_pricing_screen.dart';
+import 'package:eastri_customer_app/views/screens/pricing_screen.dart/pricing_tab_screen.dart';
+import 'package:eastri_customer_app/views/screens/pricing_screen.dart/steam_iron_pricing_screen.dart';
 import 'package:eastri_customer_app/views/screens/select_location_screen.dart';
 import 'package:eastri_customer_app/views/screens/start_screen.dart';
+import 'package:eastri_customer_app/views/screens/wallet_screen/wallet_screen1.dart';
 import 'package:get/get.dart';
 
 class AppRoutes {
@@ -15,6 +20,11 @@ class AppRoutes {
   static const String startScreen = '/startScreen';
   static const String dashboardScreen = '/dashboardScreen';
   static const String addAddressScreen = '/addAddressScreen';
+  static const String wallet1Screen = '/wallet1Screen';
+  static const String pricingTabScreen = '/pricingTabScreen';
+  static const String ironPricingScreen = '/ironPricingScreen';
+  static const String steamIronPricingScreen = '/steamIronPricingScreen';
+  static const String checkoutScreen = '/checkoutScreen';
 
   static final List<GetPage> routes = [
     GetPage(name: onBoardScreen, page: () => OnBoardScreen()),
@@ -24,5 +34,10 @@ class AppRoutes {
     GetPage(name: startScreen, page: () => StartScreen()),
     GetPage(name: dashboardScreen, page: () => DashboardScreen()),
     GetPage(name: addAddressScreen, page: () => AddAddressScreen()),
+    GetPage(name: wallet1Screen, page: () => WalletScreen1()),
+    GetPage(name: pricingTabScreen, page: () => PricingTabScreen()),
+    GetPage(name: ironPricingScreen, page: () => IronPricingScreen()),
+    GetPage(name: steamIronPricingScreen, page: () => SteamIronPricingScreen()),
+    GetPage(name: checkoutScreen, page: () => CheckoutScreen()),
   ];
 }
--- lib/views/screens/dashboard_screen/add_address_screen.dart
+++ lib/views/screens/dashboard_screen/add_address_screen.dart
@@ -1,6 +1,7 @@
 import 'package:eastri_customer_app/res/appColors/app_colors.dart';
 import 'package:eastri_customer_app/views/screens/dashboard_screen/widgets/address_container.dart';
 import 'package:eastri_customer_app/views/screens/dashboard_screen/widgets/saved_address_container.dart';
+import 'package:eastri_customer_app/views/widgets/blurred_gradient_container.dart';
 import 'package:eastri_customer_app/views/widgets/custom_bottom_navigation_bar.dart';
 import 'package:eastri_customer_app/views/widgets/greeting_section.dart';
 import 'package:eastri_customer_app/views/widgets/translate_contianer.dart';
@@ -17,84 +18,92 @@ class AddAddressScreen extends StatefulWidget {
 class _AddAddressScreenState extends State<AddAddressScreen> {
   @override
   Widget build(BuildContext context) {
-    // double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: AppColors.dashboardBg,
-      body: SafeArea(
-        child: Column(
-          children: [
-            GreetingSection(
-              icon: Icons.arrow_back,
-              onIconPressed: () {
-                Get.offNamed('/dashboardScreen');
-              },
-            ),
-            TranslateContianer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AddressContainer(),
-                  const SizedBox(height: 20),
-                  Row(
+      body: Stack(
+        children: [
+          BlurredGradientContainer(
+            height: screenHeight * 0.2,
+            child: Container(),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                GreetingSection(
+                  icon: Icons.arrow_back,
+                  onIconPressed: () {
+                    Get.offNamed('/dashboardScreen');
+                  },
+                ),
+                TranslateContianer(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Expanded(
-                        child: Divider(
-                          color: Color(0xFFB2AEAE),
-                          thickness: 1,
-                        ),
+                      AddressContainer(),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          const Expanded(
+                            child: Divider(
+                              color: Color(0xFFB2AEAE),
+                              thickness: 1,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          const Text(
+                            'SAVED ADDRESSES',
+                            style: TextStyle(
+                              color: Color(0xFF2C6A89),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          const Expanded(
+                            child: Divider(
+                              color: Color(0xFFB2AEAE),
+                              thickness: 1,
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(width: 8),
-                      const Text(
-                        'SAVED ADDRESSES',
-                        style: TextStyle(
-                          color: Color(0xFF2C6A89),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      const SizedBox(height: 20),
+                      SavedAddressContainer(
+                        title: 'Home',
+                        address:
+                            'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
+                        endIcon: Icons.home_outlined,
                       ),
-                      const SizedBox(width: 8),
-                      const Expanded(
-                        child: Divider(
-                          color: Color(0xFFB2AEAE),
-                          thickness: 1,
-                        ),
+                      const SizedBox(height: 10),
+                      SavedAddressContainer(
+                        title: 'Home',
+                        address:
+                            'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
+                        endIcon: Icons.home_outlined,
+                      ),
+                      const SizedBox(height: 10),
+                      SavedAddressContainer(
+                        title: 'Home',
+                        address:
+                            'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
+                        endIcon: Icons.home_outlined,
+                      ),
+                      const SizedBox(height: 10),
+                      SavedAddressContainer(
+                        title: 'Home',
+                        address:
+                            'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
+                        endIcon: Icons.home_outlined,
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  SavedAddressContainer(
-                    title: 'Home',
-                    address:
-                        'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
-                    endIcon: Icons.home_outlined,
-                  ),
-                  const SizedBox(height: 10),
-                  SavedAddressContainer(
-                    title: 'Home',
-                    address:
-                        'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
-                    endIcon: Icons.home_outlined,
-                  ),
-                  const SizedBox(height: 10),
-                  SavedAddressContainer(
-                    title: 'Home',
-                    address:
-                        'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
-                    endIcon: Icons.home_outlined,
-                  ),
-                  const SizedBox(height: 10),
-                  SavedAddressContainer(
-                    title: 'Home',
-                    address:
-                        'Durlabh Niwas, 1/461, Dr Baba Saheb Chawk, New Delhi',
-                    endIcon: Icons.home_outlined,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
